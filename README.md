064594941b0a337d56b29923415d78ee0b269cee
Brandy <CorwinBM5021@uwec.edu>
2024-12-19 14:06:33 -0600

    added qualimap to github and ran Snkafile_star

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
  For this project, our goal is to determine if miracidia from the liver and the intestines are transcriptomically different. If they are, we also want to identify any differentially expressed genes.
 
 ## Reduced Data vs Whole Data
- Since the data was very large, I did not have enough time to run it on the whole dataset. The reduced dataset has 10% of the data for each file. Anything with "_reduced" uses the reduced data.
+ Since the data was very large, I did not have enough time to run it on the whole dataset. The reduced dataset has 10% of the data for each file. Anything with "_reduced" uses the reduced data.  
+ The pipeline_star section is on the whole dataset, but only with the star alignment.
 
 ## Ways to Run the Analysis
  There are two ways of running this analysis: in a jupyter notebook or through snakemake.
---
+++ pipeline_star/diffexp_star.R
@@ -0,0 +1,95 @@
+library(tidyverse)
+library(broom)
+library(DESeq2, lib.loc="/data/users/corwinbm5021/.conda/envs/biol343_project/lib/R/library/")
+
+
+# For star
+star_counts_df <- read_tsv("star_counting/counts/star_counts.tsv", comment = "#") |>
+             mutate(across(where(is.numeric), as.integer))
+write.csv(star_counts_df, "star_count_matrices/star_counts_df.csv")
+
+star_counts_summary <- star_counts_df |>
+    select(Geneid, contains('star.bam')) |>
+    rename_with(~str_remove(., "star_counting/dedup/star.bam:"), everything()) |>
+    rowwise() |>
+    mutate(total_counts = sum(c_across(where(is.numeric)), na.rm = T)) |>
+    filter(total_counts >= 10)
+write.csv(star_counts_summary, "star_count_matrices/star_counts_summary.csv")
+
+star_sample_summary <- star_counts_df |>
+    select(Geneid, contains('star.bam')) |>
+    rename_with(~str_remove(., "star_counting/dedup/star.bam:"), everything()) |>
+    pivot_longer(-Geneid, names_to = 'sample', values_to = 'count') |>
+    filter(count > 0) |>
+    group_by(Geneid) |>
+    tally() |>
+    filter(n <= 3)
+write.csv(star_sample_summary, "star_count_matrices/star_sample_summary.csv")
+
+star_genes_to_remove = star_sample_summary$Geneid
+
+star_counts_filt <- star_counts_summary |>
+    filter(!Geneid %in% star_genes_to_remove) |>
+    arrange(Geneid) |>
+    select(-total_counts)
+
+star_counts_m <- star_counts_filt |>
+    select(-Geneid) |>
+    as.matrix()
+rownames(star_counts_m) <- star_counts_filt$Geneid
+write.csv(star_counts_m, "star_count_matrices/star_counts_m.csv")
+
+star_dists <- dist(t(star_counts_m))
+
+star_dists_df <- as.matrix(star_dists) |>
+    as_tibble(rownames = 'sample')
+
+star_dist_plot <- star_dists_df |>
+    pivot_longer(-sample, names_to = 'comp', values_to = 'dist') |>
+    ggplot(aes(x = sample, y = comp, fill = dist)) +
+    geom_tile() +
+    scale_fill_viridis_c() +
+    coord_equal() +
+    NULL
+ggsave("star_plots/star_dist_plot_before_diffexp.png")
+
+star_pca_fit <- t(log10(star_counts_m + 1)) |> 
+  prcomp(scale = TRUE)
+
+star_pca_plot_before_deseq2 <- star_pca_fit |>
+    augment(t(star_counts_m)) |>
+    dplyr::rename(sample = .rownames) |>
+    mutate(sample = str_remove(sample, '-[0-9]+')) |>
+    ggplot(aes(.fittedPC1, .fittedPC2, color = sample)) + 
+    geom_point(size = 5)
+ggsave("star_plots/star_pca_before_deseq2.png")
+
+star_metadata <- data.frame(sample_id = colnames(star_counts_m)) |>
+    mutate(tissue = str_sub(sample_id, 1, 3),
+           rep = str_sub(sample_id, 5))
+rownames(star_metadata) <- star_metadata$sample_id
+star_metadata <- select(star_metadata, -sample_id)
+
+all(rownames(star_metadata) == colnames(star_counts_m))
+
+star_dds <- DESeqDataSetFromMatrix(countData = star_counts_m,
+                              colData = star_metadata,
+                              design = ~ tissue)
+star_dds <- DESeq(star_dds)
+
+star_res <- results(star_dds)
+
+star_volcano_data <- as_tibble(star_res, rownames = 'gene_id')
+
+star_volcano_plot <- star_volcano_data |> 
+    ggplot(aes(x = log2FoldChange, y = -log10(padj))) +
+    geom_point() +
+    geom_vline(xintercept = 2) +
+    geom_vline(xintercept = -2)
+ggsave("star_plots/star_volcano_plot.png")
+
+star_vsd <- varianceStabilizingTransformation(star_dds)
+write.csv(assay(star_vsd), "star_count_matrices/star_vsd.csv")
+
+star_pca_after_deseq2 <- plotPCA(star_vsd, intgroup = 'tissue')
+ggsave("star_plots/star_pca_after_deseq2.png")
